407701da0bd8f89d39e5ecf5d927a34095ae7aff
ANTK2 <laczi.fekete@gmail.com>
2024-06-03 01:43:15 +0200

    male upravy

--- parts/footer.php
+++ parts/footer.php
@@ -1,112 +1,97 @@
-<footer
-          class="text-center text-lg-start text-dark"
-          style="background-color: #ECEFF1"
-          >
-    <section
-             class="d-flex justify-content-between p-4 text-white"
-             style="background-color: #0b03fc"
-             >
-      <div class="me-5">
-        <span>Get connected with us on social networks:</span>
-      </div>
-      <div>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-google"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-instagram"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-linkedin"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-github"></i>
-        </a>
-      </div>
+<footer class="text-center text-lg-start text-dark" style="background-color: #ECEFF1">
+    <section class="d-flex justify-content-between p-4 text-white" style="background-color: #0b03fc">
+        <div class="me-5">
+            <span>Spojte sa s nami na sociálnych sieťach:</span>
+        </div>
+        <div>
+            <a href="https://facebook.com" class="text-white me-4">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://twitter.com" class="text-white me-4">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="https://google.com" class="text-white me-4">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="https://instagram.com" class="text-white me-4">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="https://linkedin.com" class="text-white me-4">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com" class="text-white me-4">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
     </section>
     <section class="">
-      <div class="container text-center text-md-start mt-5">
-        <div class="row mt-3">
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold">Company name</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              Here you can use rows and columns to organize your footer
-              content. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit.
-            </p>
-          </div>
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold">Products</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              <a href="#!" class="text-dark">MDBootstrap</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">MDWordPress</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">BrandFlow</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Bootstrap Angular</a>
-            </p>
-          </div>
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold">Useful links</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              <a href="#!" class="text-dark">Your Account</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Become an Affiliate</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Shipping Rates</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Help</a>
-            </p>
-          </div>
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <h6 class="text-uppercase fw-bold">Contact</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-          </div>
+        <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">Noviny Slovakia</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <p>
+                        NovinySlovakia je vašim zdrojom aktuálnych správ a udalostí z celého sveta. Zostaňte informovaní v reálnom čase o rôznych témach.
+                    </p>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">Kategórie</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <p>
+                        <a href="index" class="text-dark">Najnovšie Správy</a>
+                    </p>
+                    <p>
+                        <a href="zdravie" class="text-dark">Zdravie</a>
+                    </p>
+                    <p>
+                        <a href="sport" class="text-dark">Šport</a>
+                    </p>
+                    <p>
+                        <a href="politika" class="text-dark">Politika</a>
+                    </p>
+                    <p>
+                        <a href="technologie" class="text-dark">Technológie</a>
+                    </p>
+                    <p>
+                        <a href="zivotne-prostredie" class="text-dark">Životné Prostredie</a>
+                    </p>
+                    <p>
+                        <a href="ekonomika" class="text-dark">Ekonomika</a>
+                    </p>
+                </div>
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">Užitočné odkazy</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <p>
+                        <a href="#!" class="text-dark">Váš účet</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-dark">Staňte sa affiliate</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-dark">Cenové plány</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-dark">Pomoc</a>
+                    </p>
+                </div>
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <h6 class="text-uppercase fw-bold">Kontakt</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <p><i class="fas fa-home mr-3"></i> Bratislava, Slovensko</p>
+                    <p><i class="fas fa-envelope mr-3"></i> info@novinyslovakia.sk</p>
+                    <p><i class="fas fa-phone mr-3"></i> +421 123 456 789</p>
+                    <p><i class="fas fa-print mr-3"></i> +421 987 654 321</p>
+                </div>
+            </div>
         </div>
-      </div>
     </section>
-    <div
-         class="text-center p-3"
-         style="background-color: rgba(0, 0, 0, 0.2)"
-         >
-      © 2020 Copyright:
-      <a class="text-dark" href="https://mdbootstrap.com/"
-         >MDBootstrap.com</a
-        >
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2024 Autorské práva:
+        <a class="text-dark" href="https://novinyslovakia.sk/">NovinySlovakia.sk</a>
     </div>
-  </footer>
+</footer>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js" integrity="sha512-TPh2Oxlg1zp+kz3nFA0C5vVC6leG/6mm1z9+mA81MI5eaUVqasPLO8Cuk4gMF4gUfP5etR73rgU/8PNMsSesoQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
